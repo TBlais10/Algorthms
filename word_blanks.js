f182b5506ae9255aad29d91e7186882f19af314c
TBlais10 <blaislighting@gmail.com>
2021-07-13 17:21:32 -0400

    This is another FFC challenge in basic JavaScript

---
+++ word_blanks.js
@@ -0,0 +1,13 @@
+var myNoun = "dog"
+var myAdjective = "big"
+var myVerb = "ran"
+var myAdverb = "quickly"
+
+var wordBlanks = `I saw a ${myNoun} and it was really ${myAdjective}. I watched as it ${myVerb} ${myAdverb} through the yard.`
+console.log(wordBlanks)
+/*wordBlanks should be a string.
+You should not change the values assigned to myNoun, myVerb, myAdjective or myAdverb.
+You should not directly use the values dog, ran, big, or quickly to create wordBlanks.
+wordBlanks should contain all of the words assigned to the variables myNoun, 
+myVerb, myAdjective and myAdverb separated by non-word characters 
+(and any additional words in your madlib)*/
