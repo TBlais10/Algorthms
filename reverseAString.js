4103cc160f757ddd9c7ebc53ae9381732bc05f2d
TBlais10 <blaislighting@gmail.com>
2021-07-20 22:25:37 -0400

    This was the second beginner FCC algorithm challenges

---
+++ reverseAString.js
@@ -0,0 +1,12 @@
+//challenge 2
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/reverse-a-string
+
+// function reverseString(str) {
+//     let testArr = ""
+//     for (let i = str.length - 1; i >= 0 ; i--) {
+//       testArr += str[i]
+//     }
+//     return testArr;
+//   }
+
+//   console.log(reverseString("hello"))
