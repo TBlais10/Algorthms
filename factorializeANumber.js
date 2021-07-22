619874de24c85db960016dc3b2a4f3a0ecf615fb
TBlais10 <blaislighting@gmail.com>
2021-07-21 22:15:53 -0400

    This is another FCC basic algorithm challenge

---
+++ factorializeANumber.js
@@ -0,0 +1,18 @@
+//challenge 3
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/factorialize-a-number
+
+// function factorialize(num) {
+//     if (num < 0) {
+//         return `This number is a negative number`
+//     }
+//     else if (num === 0) {
+//         return 1
+//     } else {
+//         let arr = 1
+//         for (let i = 1; i <= num; i++)
+//             arr *= i
+//         return arr
+//     }
+// }
+
+// console.log(factorialize(5))
