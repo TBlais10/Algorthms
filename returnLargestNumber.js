f7be34980eea07f03e43ac1478c0ca24eb60dcda
TBlais10 <blaislighting@gmail.com>
2021-07-23 23:40:59 -0400

    Another FCC basic algorithm challenge

---
+++ returnLargestNumber.js
@@ -0,0 +1,17 @@
+//challenge 5
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/return-largest-numbers-in-arrays
+
+// function largestOfFour(arr) {
+//     let largest = [0,0,0,0]
+//     for (let i = 0; i < arr.length; i++) {
+//       for (let j = 1; j < arr[i].length; j++) {
+//         if (arr[i][j] > largest[i]) {
+//           largest[i] = arr[i][j]
+
+//         }
+//       }
+//     }
+//     return largest;
+//   }
+
+//   console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]))
