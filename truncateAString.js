47e7e25618a6a1024e82ce719dc8da44b582492c
TBlais10 <blaislighting@gmail.com>
2021-07-25 16:03:57 -0400

    This is another basic FCC algorithm challenges

---
+++ truncateAString.js
@@ -0,0 +1,15 @@
+//challenge 8
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/truncate-a-string
+
+// function truncateString(str, num) {
+//     const arr = `...`
+//     if (str.length <= num){
+//       return str
+//     }
+//     do { 
+//          return str.slice(0, num) + arr;
+//     }
+//   while (num = str.length)
+//   }
+
+//   console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length))
