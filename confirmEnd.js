db2a8bbc9f950bf1d828b4f2d1ebe6aa082703af
TBlais10 <blaislighting@gmail.com>
2021-07-24 21:04:23 -0400

    Another completed  FCC challenge

---
+++ confirmEnd.js
@@ -0,0 +1,9 @@
+//challenge 6
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/confirm-the-ending
+
+//   function confirmEnding(str, target) {
+
+//     return str.slice(str.length - target.length) === target
+//     }
+
+//     console.log(confirmEnding("Connor", "n"))
