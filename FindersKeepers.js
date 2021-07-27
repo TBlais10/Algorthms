80ee0a9443a4524578db98effd9997fba1e6e62d
TBlais10 <blaislighting@gmail.com>
2021-07-27 00:35:20 -0400

    Another JavaScript  challenge from FCC

---
+++ FindersKeepers.js
@@ -0,0 +1,14 @@
+//challenge 9
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/finders-keepers
+
+// function findElement(arr, func) {
+//     let num = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         num = arr[i]
+//         if (func(num)) {
+//             return num
+//         }
+
+//     }
+// }
