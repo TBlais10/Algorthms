52ea914651a48ef4062bd232ad7ba7faa0f89b40
TBlais10 <blaislighting@gmail.com>
2021-07-28 14:17:45 -0400

    Another basic Algorithm challenge from FCC.

---
+++ TitleCase.js
@@ -0,0 +1,14 @@
+//Challenge 11
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/title-case-a-sentence
+
+// function titleCase(str) {
+//     const arr = str.toLowerCase().split(" ")
+//      for (let i = 0; i < arr.length; i++){
+
+//      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].substring(1);
+
+//      }
+//    return arr.join(" ")
+//    }
+
+//    console.log(titleCase("I'm a little tea pot"))
