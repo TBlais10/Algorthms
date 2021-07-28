befcfaf70ce5f887f665d16d41e551a148b5a702
TBlais10 <blaislighting@gmail.com>
2021-07-27 22:26:34 -0400

    Another basic algorithm FCC challenge.

---
+++ BooWho.js
@@ -0,0 +1,12 @@
+//Challenge 10
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/boo-who
+
+// function booWho(bool) {
+//     if (bool === true || bool === false) {
+//       return true
+//     } else {
+//       return false
+//     }
+
+//   }
+//   console.log(booWho(false))
