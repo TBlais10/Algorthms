e5c5d6f25eb68b4c845acc00356ee890891dd235
TBlais10 <blaislighting@gmail.com>
2021-07-29 22:45:41 -0400

    Another basic Algorithm FCC JS  challenge.

---
+++ SliceAndSplice.js
@@ -0,0 +1,13 @@
+//Challenge 12
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/slice-and-splice
+
+// function frankenSplice(arr1, arr2, n) {
+//   let test = arr2.slice()
+//   for (let i = 0; i < arr1.length; i++) {
+//     test.splice(n, 0, arr1[i]);
+//     n++
+//   }
+//   return test
+// }
+
+// console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1))
