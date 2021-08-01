10147621e343dab9dc3e2b6ac00db4ecb9ffeb50
TBlais10 <blaislighting@gmail.com>
2021-07-31 23:03:33 -0400

    Another basic JS  algorithm challenge from FCC.

---
+++ mutations.js
@@ -0,0 +1,15 @@
+//challenge 15
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/mutations
+
+// function mutation(arr) {
+//   let test = arr[1].toLowerCase()
+//   let result = arr[0].toLowerCase()
+
+//   for (let i = 0; i < test.length; i++) {
+//     if (result.indexOf(test[i]) < 0) return false
+//   }  
+//   return true
+// }
+
+
+// console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]))
